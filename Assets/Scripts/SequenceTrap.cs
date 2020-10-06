8d65c0bc5de70517dad638cd0d91a73c2f105509
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-10-06 16:36:05 -0400

    Sequence Trap Added

---
+++ Assets/Scripts/SequenceTrap.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceTrap : MonoBehaviour
+{
+    public static string correctSequence = "213";
+
+    public static string playerCode = "";
+
+    public static int totalCode = 0;
+
+    private void Update()
+    {
+        if (totalCode == 3)
+        {
+            if (playerCode == correctSequence)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                playerCode = "";
+                totalCode = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCode += gameObject.name;
+            totalCode += 1;
+        }
+    }
+}
