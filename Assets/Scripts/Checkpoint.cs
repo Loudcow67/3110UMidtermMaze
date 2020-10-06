8cd894bec25c19ba987f220f5784aab39a0f6ea0
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-10-06 13:50:36 -0400

    Respawn Added

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Transform checkpoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.transform.position = checkpoint.transform.position;
+            Physics.SyncTransforms();
+            print("touched");
+        }
+    }
+}
