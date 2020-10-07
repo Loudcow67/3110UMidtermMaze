1d1623f272dcb4447075b825f1a6246787354aaf
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-10-07 14:52:44 -0400

    Dll and Checkpoint Time added

---
+++ Assets/Scripts/DllManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class DllManager : MonoBehaviour
+{
+    const string DLL_NAME = "3110UMidtermDLL";
+
+    [DllImport(DLL_NAME)]
+    private static extern void ResetLogger();
+
+    [DllImport(DLL_NAME)]
+    private static extern void SaveCheckpointTime(float RTBC);
+
+    [DllImport(DLL_NAME)]
+    private static extern float GetTotalTime();
+
+    [DllImport(DLL_NAME)]
+    private static extern float GetCheckpointTime(int index);
+
+    [DllImport(DLL_NAME)]
+    private static extern int GetNumCheckpoint();
+
+    float lastTime = 0.0f;
+
+    public void SaveTime(float checkpointTime)
+    {
+        SaveCheckpointTime(checkpointTime);
+    }
+
+    public float LoadTime(int index)
+    {
+        if (index >= GetNumCheckpoint())
+        {
+            return -1.0f;
+        }
+        else
+        {
+            return GetCheckpointTime(index);
+        }
+    }
+
+    public float LoadTotalTime()
+    {
+        return GetTotalTime();
+    }
+
+    public void ResetLoggerFunction()
+    {
+        ResetLogger();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTime = Time.time;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            float currentTime = Time.time;
+            float checkpointTime = currentTime - lastTime;
+            lastTime = currentTime;
+            
+            SaveTime(checkpointTime);
+            Debug.Log(LoadTime(0)); 
+            Debug.Log(LoadTotalTime());
+        }
+    }
+
+    private void OnDestroy()
+    {
+        ResetLoggerFunction();
+    }
+}
